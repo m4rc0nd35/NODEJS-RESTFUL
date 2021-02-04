be4430c990f5102c156a7b51388cdba9be8e99ca
root <root@DESKTOP-MJ0UATN.localdomain>
2021-02-03 23:25:38 -0300

    add jwt

--- app.js
+++ app.js
@@ -26,11 +26,6 @@ app.use((req, res, next) =>{
     next(erro);
 });
 app.use((error, req, res, next) =>{
-    res.status(error.status || 500);
-    return res.send({
-        erro: {
-            mensagem: error.message
-        }
-    });
+    return res.status(error.status || 500).send({erro: {mensagem: error.message}});
 });
 module.exports = app;
--- routes/authentication.js
+++ routes/authentication.js
@@ -6,26 +6,38 @@ const jwt = require("jsonwebtoken");
 router.post('/', (req, res, next)=>{
     mysql.getConnection((error, conn)=>{
         if(error) return res.status(500).send({ error: error});
-        // conn.query('CALL SP_LOGIN(?, ?, @token, @success); SELECT @token, @success;',
-        //     [req.body.username, req.body.password],
-        //     (error, fetchOne, field)=>{
-        //         conn.release();
-        //         if(error){
-        //             conn.rollback();
-        //             return res.status(500).send({ error: error});
-        //         }
-        //         return res.status(202).send(fetchOne);
-        //     }
-        // );
-    });
-    const token = jwt.sign({
-        id_user: req.body.username
-    },
-    process.env.SECRET_KEY,
-    );
-    return res.status(200).send({
-        mensagem: 'Autenticado com sucesso!',
-        token: token
+        conn.query('CALL SP_LOGIN(?, ?, @token, @success); SELECT @token, @success;',
+            [
+                req.body.username, 
+                req.body.password
+            ],
+            (error, fetchOne, field)=>{
+                conn.release();
+                if(error){
+                    conn.rollback();
+                    return res.status(500).send({ error: error});
+                }
+                if(fetchOne[1][0]["@success"]){
+                    const token = jwt.sign({
+                        id_user: fetchOne[1][0]["@token"]
+                    },
+                    process.env.SECRET_KEY,{
+                        expiresIn: 600
+                    }
+                    );
+                    return res.status(202).send({
+                        mensagem: 'Autenticado com sucesso!',
+                        token: token,
+                        fetchOne: fetchOne[1][0]
+                    });
+                }else{
+                    return res.status(401).send({
+                        mensagem: 'Autentição negada!',
+                        token: false
+                    });
+                }
+            }
+        );
     });
 })
 module.exports = router;
