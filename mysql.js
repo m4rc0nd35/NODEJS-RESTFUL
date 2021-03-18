37b725cb364df1c42ee5d8f71614e084945c0dd3
root <root@DESKTOP-UD2KKHP.localdomain>
2021-03-17 23:45:03 -0300

    changed mysql

--- mysql.js
+++ mysql.js
@@ -1,13 +1,29 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    timezone: '+00:00',
-    multipleStatements: true
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DB,
+	host: process.env.MYSQL_HOST,
+	port: process.env.MYSQL_PORT,
+	timezone: '+00:00',
+	multipleStatements: true
 });
-
-exports.pool = pool;
+exports.execute = (query, params = []) => {
+	return new Promise((resolve, reject) => {
+		pool.getConnection((error, conn) => {
+			if (error)
+				reject(error);
+			else {
+				conn.query(query, params, (error, result, field) => {
+					conn.release();
+					if (error)
+						reject(error);
+					else {
+						resolve(result);
+					}
+				});
+			}
+		});
+	});
+}
